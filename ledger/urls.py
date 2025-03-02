9c2b4342bd184f116be79c9ebef3f2844aa32e31
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:38:06 +0800

    Modified views inside path to match the renamed views

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RecipeListView, RecipeView
 
 urlpatterns = [
-    path('recipes/list', RecipeListView, name='list'),
-    path('recipe/<int:pk>', RecipeView, name='<str:name>'),
+    path('recipes/list', recipe_list, name='list'),
+    path('recipe/<int:pk>', recipe, name='<str:name>'),
 ]
 app_name = "ledger"
