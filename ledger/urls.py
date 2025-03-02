eaed8042e3e9b48b06a906ed4a4545f6f60c33c8
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:19:11 +0800

    Modified urlpatterns to comply with the models

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import recipe_list, recipe_1, recipe_2
+from .views import RecipeListView, RecipeView
 
 urlpatterns = [
-    path('recipes/list', recipe_list, name='list'),
-    path('recipe/1', recipe_1, name='1'),
-    path('recipe/2', recipe_2, name='2'),
+    path('recipes/list', RecipeListView, name='list'),
+    path('recipe/<int:pk>', RecipeView, name='<str:name>'),
 ]
 app_name = "ledger"
