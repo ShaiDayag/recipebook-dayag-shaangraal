b6bde164ac95a6d6adc3ee1a333b61d164605bd1
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:11:45 +0800

    Modified __str__ and get_absolute_url

--- ledger/models.py
+++ ledger/models.py
@@ -11,8 +11,14 @@ class Recipe(models.Model):
 # RecipeIngredient model with ingredient and recipe fields as foreign keys
 class RecipeIngredient(models.Model):
     quantity = models.DecimalField(max_digits=50)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL)
-    recipe = models.ForeignKey(Recipe, on_delete=models.SET_NULL)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL, related_name="recipe")
+    recipe = models.ForeignKey(Recipe, on_delete=models.SET_NULL, related_name="ingredients")
+
+def __str__(self):
+    return '{}: due on {} unit(s)'.format(self.name, self.due_date)
+
+def get_absolute_url(self):
+    return reverse('task_detail', args=[str(self.name)])
 
 # class Task(models.Model):
 #     name = models.CharField(max_length=100)
