aafb1e58edadefc7e9a84ab9318fd800b15988ad
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:20:34 +0800

    Deleted the previous views, added the ListView equivalent

--- ledger/views.py
+++ ledger/views.py
@@ -1,72 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 def recipe_list(request):
+    recipes = Recipe.objects.all()
     recipe_list = {
-        "recipes": [
-            {
-                "name": "Recipe 1",
-                "ingredients": [
-                    {
-                        "name": "tomato",
-                        "quantity": "3pcs"
-                    },
-                    {
-                        "name": "onion",
-                        "quantity": "1pc"
-                    },
-                    {
-                        "name": "pork",
-                        "quantity": "1kg"
-                    },
-                    {
-                        "name": "water",
-                        "quantity": "1L"
-                    },
-                    {
-                        "name": "sinigang mix",
-                        "quantity": "1 packet"
-                    }
-                ],
-                "link": "/recipe/1"
-            },
-            {
-                "name": "Recipe 2",
-                "ingredients": [
-                    {
-                        "name": "garlic",
-                        "quantity": "1 head"
-                    },
-                    {
-                        "name": "onion",
-                        "quantity": "1pc"
-                    },
-                    {
-                        "name": "vinegar",
-                        "quantity": "1/2cup"
-                    },
-                    {
-                        "name": "water",
-                        "quanity": "1 cup"
-                    },
-                    {
-                        "name": "salt",
-                        "quantity": "1 tablespoon"
-                    },
-                    {
-                        "name": "whole black peppers",
-                        "quantity": "1 tablespoon"
-                    },
-                    {
-                        "name": "pork",
-                        "quantity": "1 kilo"
-                    }
-                ],
-                "link": "/recipe/2"
-            }
-        ]
+        "recipes": recipes
     }
     return render(request, "list.html", recipe_list)
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'list.html' # this is the default value
+    
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html' #this is the default value
 
 def recipe_1(request):
     recipe_1 = {
