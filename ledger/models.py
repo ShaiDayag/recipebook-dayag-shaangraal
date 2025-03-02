49bf1250f6ef60914bc09b504eefd2e4c508e36f
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 20:51:48 +0800

    Added commas to fix errors

--- ledger/models.py
+++ ledger/models.py
@@ -12,13 +12,13 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
         related_name="recipe"
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
         related_name="ingredients"
     )
 
