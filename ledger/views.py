6b9bd4e37943ffbc1cc9c91d38f9eb5e2378902f
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:43:09 +0800

    Modified views back into CBV

--- ledger/views.py
+++ ledger/views.py
@@ -2,10 +2,10 @@ from .models import Recipe, RecipeIngredient
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-class recipe_list(ListView):
+class RecipeListView(ListView):
     model = Recipe
     template_name = 'list.html'
     
-class recipe(DetailView):
+class RecipeView(DetailView):
     model = RecipeIngredient
     template_name = 'recipe.html'
