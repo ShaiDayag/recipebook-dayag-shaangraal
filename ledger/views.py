d142c21633deeb802d26c6b6a50f2720c579eb63
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:54:30 +0800

    Modified import statements

--- ledger/views.py
+++ ledger/views.py
@@ -1,11 +1,10 @@
 from .models import Recipe, RecipeIngredient
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from .views import ListView, DetailView
 
 class RecipeListView(ListView):
     model = Recipe
-    template_name = 'list.html'
+    template_name = 'ledger/templates/list.html'
     
 class RecipeView(DetailView):
     model = RecipeIngredient
-    template_name = 'recipe.html'
+    template_name = 'ledger/templates/recipe.html'
