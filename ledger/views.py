a270e533bde02af738f4a4beeb79a3ce4cb84f0c
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 21:42:12 +0800

    Edited template_name for both views to fix errors

--- ledger/views.py
+++ ledger/views.py
@@ -1,11 +1,11 @@
-from .models import Recipe, RecipeIngredient
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from .models import Recipe, RecipeIngredient
 
 class RecipeListView(ListView):
     model = Recipe
-    template_name = 'ledger/templates/list.html'
+    template_name = 'list'
     
 class RecipeView(DetailView):
     model = RecipeIngredient
-    template_name = 'ledger/templates/recipe.html'
+    template_name = 'recipe'
