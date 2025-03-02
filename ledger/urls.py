65732bf94a5bfb6f955d749d430d23695d7c89b9
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:42:33 +0800

    Modified views in paths into CBV

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RecipeListView, RecipeView
 
 urlpatterns = [
-    path('recipes/list', recipe_list, name='list'),
-    path('recipe/<int:pk>', recipe, name='<str:name>'),
+    path('recipes/list', RecipeListView.as_view(), name='list'),
+    path('recipe/<int:pk>', RecipeView.as_view(), name='<str:name>'),
 ]
 app_name = "ledger"
