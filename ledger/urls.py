555726df80f3be0c104d3cf4d83e73f33ce9074d
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 20:40:07 +0800

    Modified the path for recipe

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,6 +3,6 @@ from .views import RecipeListView, RecipeView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='list'),
-    path('recipe/<int:pk>', RecipeView.as_view(), name='<str:name>'),
+    path('recipe/<int:pk>', RecipeView.as_view(), name='recipe'),
 ]
 app_name = "ledger"
