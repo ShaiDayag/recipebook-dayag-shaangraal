0b1c1150603c881ae41d66160abb139d0c75509a
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-02 19:35:55 +0800

    Renamed RecipeListView to recipe_list and RecipeView to recipe

--- ledger/views.py
+++ ledger/views.py
@@ -2,10 +2,10 @@ from .models import Recipe, RecipeIngredient
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-class RecipeListView(ListView):
+class recipe_list(ListView):
     model = Recipe
     template_name = 'list.html'
     
-class RecipeView(DetailView):
+class recipe(DetailView):
     model = RecipeIngredient
     template_name = 'recipe.html'
