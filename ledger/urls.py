d4715d6f8ecc77034fdf5d09a50fa73191e7a820
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-03 11:33:47 +0800

    Removed .html from the pathnames

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,6 +3,6 @@ from .views import RecipeListView, RecipeView
 
 app_name = "ledger"
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name='list.html'),
-    path('recipe/<int:pk>/', RecipeView.as_view(), name='recipe.html'),
+    path('recipes/list/', RecipeListView.as_view(), name='list'),
+    path('recipe/<int:pk>/', RecipeView.as_view(), name='recipe'),
 ]
