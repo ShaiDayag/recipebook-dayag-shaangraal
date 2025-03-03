7b33e5889185b3385f6505cee7db6a915a0dfb75
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-03 10:10:35 +0800

    Added a / to the end of the path for recipe instances to fix an error

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,5 +4,5 @@ from .views import RecipeListView, RecipeView
 app_name = "ledger"
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='list.html'),
-    path('recipe/<int:pk>', RecipeView.as_view(), name='recipe.html'),
+    path('recipe/<int:pk>/', RecipeView.as_view(), name='recipe.html'),
 ]
