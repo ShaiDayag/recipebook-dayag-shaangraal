750a6468a8b2ba6d979697f45f31013a939a90af
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-03 09:02:33 +0800

    Placed app_name on top

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import RecipeListView, RecipeView
 
+app_name = "ledger"
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='list.html'),
     path('recipe/<int:pk>', RecipeView.as_view(), name='recipe.html'),
 ]
-app_name = "ledger"
