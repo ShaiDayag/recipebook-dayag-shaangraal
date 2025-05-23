aba0496f915d952f3c9bbeb823a1d24e513aefb3
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-23 21:30:11 +0800

    Added RecipeImage model and remigrated themodels

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        max_length=50
+    )
     short_bio = models.TextField(blank=True)
